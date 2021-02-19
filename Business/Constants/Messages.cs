aab16c80cdd149f98e596b00c3f9a651edefb4fd
Mstfzdmr54 <41597048+Mstfzdmr54@users.noreply.github.com>
2021-02-19 20:16:19 +0300

    Proje dosyası ekle.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi";
+        public static string ProducNameInvalid = "Ürün ismi geçersiz ";
+
+        public static string MaintenanceTime = "Sistem bakimda";
+        public static string ProductsListed = "Sistem Dinleniyor";
+    }
+}
